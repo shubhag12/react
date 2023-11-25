3500ae920fc64f2659997ec072e8f91766fe5b37
shubh agrwal <shubhagarwal123123@gmail.com>
2023-11-25 22:37:45 +0530

    ore tricky problems added

--- mapfilterreduce.js
+++ mapfilterreduce.js
@@ -51,3 +51,53 @@ const maxi=nums.reduce(function(acc,curr){
      return acc;
 },0);//the second value of reduce is the initail value of the accumulator
 console.log(maxi);
+
+
+
+
+
+const user=[{firstname:"shubh",lastname:"agarwal",age:22},
+          {firstname:"skund",lastname:"agarwal",age:22},
+          {
+          firstname:"yogesh",lastname:"agarwal",age:49
+          }];
+
+          //task is to find the list of the full name
+
+const ans=user.map((x)=>x.firstname+" "+x.lastname);
+console.log(ans);
+
+
+//now the second task is to print how many people are there with particular age
+
+//for example in above 22:2,49:1
+//the concept use here is of reduce 
+
+const ans1=user.reduce(function(acc,curr){
+     if(acc[curr.age]){
+          acc[curr.age]+=1;
+     }
+     else{
+          acc[curr.age]=1;
+     }
+     return acc;
+},{});
+console.log(ans1);
+
+//next task is to filter all the people and print the first namewhose age is less than 30
+
+const ans2=user.filter((x)=>x.age<30).map((x)=>x.firstname);
+
+console.log(ans2);
+
+
+//how to do the same thing with the reduce keyword 
+
+const ans3=user.reduce(function(acc,curr){
+     if(curr.age<30){
+          acc.push(curr.firstname);
+     }
+          return acc;
+     
+},[]);
+console.log(ans3);
